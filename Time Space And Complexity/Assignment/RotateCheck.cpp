7d39c6daf54d144d2f9ef7e657f6504bfdcded60
Ritwick Verma <96318158+ritwickoo1@users.noreply.github.com>
2022-01-20 02:20:20 +0530

    Update RotateCheck.cpp

--- Time Space And Complexity/Assignment/RotateCheck.cpp
+++ Time Space And Complexity/Assignment/RotateCheck.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 using namespace std;
-
-#include "solution.h"
-
+//2
+//5
+//3 6 8 9 10
+//4
+//10 20 30 1
+//Output
+//0
+//3
+/*
+Approach 1
+int arrayRotateCheck(int *input, int size)
+{
+    //Write your code here
+    int r = 0;
+    for(int i = 0,j=size-1; i < j; i++,j--){
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+    for(int i = 0; i < size-1; i++){
+        if(input[i] < input[i+1]){
+            r = i+1;
+            break;
+        }
+    }
+    if(r==0){
+        return 0;
+    }
+    return size-r;
+}
+*/
+int arrayRotateCheck(int *input, int size){
+    int left = 0;
+    int right = size - 1;
+    while (left < right) { 
+        int mid = left + (right - left) / 2;
+        if (input[mid] > input[right]) {
+            left = mid + 1; 
+        }
+        else {
+            right = mid;
+        }
+    }
+    return left;
+}
 int main()
 {
 
